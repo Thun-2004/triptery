c620b8e5aaa498b610991cfb0cd205bf4541b69a
Cussonspoon <140087747+cussonspoon@users.noreply.github.com>
2025-04-29 14:01:49 +0700

    perf : move BottomNavBar to widgets as file

---
+++ lib/presentation/widgets/bottom_navbar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.explore), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: ''),
+      ],
+    );
+  }
+}
