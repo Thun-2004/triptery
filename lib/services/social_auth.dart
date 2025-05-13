b5dd14b715040153fd9522cae7d58f0782a2849b
Thun-2004 <66011276@kmitl.ac.th>
2025-05-13 14:07:50 +0700

    delete twitter

--- lib/services/social_auth.dart
+++ lib/services/social_auth.dart
@@ -3,7 +3,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:twitter_login/twitter_login.dart';
 import 'package:triptery/data/datasources/supabase_client.dart'; 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
